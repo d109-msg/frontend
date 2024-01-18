dc894afdac66c87d9b97b25591b0e06dfd6af0ef
이창곤 <rhsdl970110@naver.com>
2024-01-19 00:50:14 +0900

    feat: s3 file 삭제, 조회, uuid upload

--- backend/src/main/java/com/ssafy/msg/article/util/S3Util.java
+++ backend/src/main/java/com/ssafy/msg/article/util/S3Util.java
@@ -1,6 +1,7 @@
 package com.ssafy.msg.article.util;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -18,14 +20,41 @@ public class S3Util {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    /**
+     * 이미지를 입력받아 s3에 업로드하고 uuid를 리턴
+     * @param multipartFile 이미지를 입력 받음
+     * @return String uuid를 리턴
+     * @throws IOException
+     */
     public String saveFile(MultipartFile multipartFile) throws IOException {
         String originalFilename = multipartFile.getOriginalFilename();
+        String fileName = originalFilename + UUID.randomUUID();
 
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(multipartFile.getSize());
         metadata.setContentType(multipartFile.getContentType());
 
-        amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
-        return amazonS3.getUrl(bucket, originalFilename).toString();
+        amazonS3.putObject(bucket, fileName, multipartFile.getInputStream(), metadata);
+        return fileName;
+    }
+
+    /**
+     * uuid를 입력받아 url을 리턴하는 함수
+     * @param uuid uuid를 입력
+     * @return String url리턴
+     *
+     */
+    public String getUrl(String uuid) {
+        return amazonS3.getUrl(bucket, uuid).toString();
+    }
+
+    /**
+     * uuid를 입력 받아 bucket에서 파일을 삭제
+     * @param fileName
+     *
+     */
+    public void deleteFile(String fileName) {
+        DeleteObjectRequest request = new DeleteObjectRequest(bucket, fileName);
+        amazonS3.deleteObject(request);
     }
 }
