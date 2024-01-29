89a1fc52f59cdc43ae1eaa1bfa52322600cb81cf
금예인 <tbc07061@naver.com>
2024-01-30 00:30:52 +0900

    fix: WebSocket 관련 버그 수정

--- backend/src/main/java/com/ssafy/msg/message/interceptor/FilterChannelInterceptor.java
+++ backend/src/main/java/com/ssafy/msg/message/interceptor/FilterChannelInterceptor.java
@@ -13,8 +13,10 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
 
+import java.security.Principal;
 import java.util.Map;
 
 // @Order: Spring Security보다 인터셉터의 우선 순위 올리기
@@ -32,20 +34,34 @@ public class FilterChannelInterceptor implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         // StompHeaderAccessor를 사용해 STOMP 헤더에 접근
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+        // StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message); => 왜 안되는지 확인 필요
+        StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        // 웹소켓 세션 ID
+        String sessionId = headerAccessor.getSessionId();
+        log.info("WebSocket session ID: " + sessionId);
+
+        Principal principal = headerAccessor.getUser();
+        log.info(message.toString());
+        log.info("Principal in preSend: " + principal);
 
         if (StompCommand.CONNECT.equals(headerAccessor.getCommand())){
             String token = headerAccessor.getFirstNativeHeader(authorization);
             if(jwtUtil.verify(token, "access-token")){
                 int id = jwtUtil.getId(token);
-                headerAccessor.setUser(StompPrincipalDto.builder()
+                StompPrincipalDto principalDto = StompPrincipalDto.builder()
                         .userId(id)
                         .userStringId(Integer.toString(id))
-                        .build());
+                        .build();
+                headerAccessor.setUser(principalDto);
             } else{
                 throw new TokenInvalidException();
             }
+            log.info("Princial CONNECT: " + String.valueOf(headerAccessor.getUser()));
+        }else if(StompCommand.SUBSCRIBE.equals(headerAccessor.getCommand())){
+            log.info("Princial SUBSCRIBE: " + String.valueOf(headerAccessor.getUser()));
         }
+
 
         return message;
     }
