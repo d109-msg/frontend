394bb38de74367249d1f056109611aac3043c506
hyunjihye <hhh259@naver.com>
2024-02-02 21:03:26 +0900

    feat: 채팅 메세지방 구현

--- frontend/src/components/MessagePage/MessageDetail.vue
+++ frontend/src/components/MessagePage/MessageDetail.vue
@@ -7,9 +7,10 @@
         <div class="message-content-box">
             <div class="message-content">
 
-                {{ userInfo.nickname }}
+
+
             </div>
-            <textarea type="text" class="message-textarea" name="" id="" cols="30" rows="10" v-model="message">
+            <textarea type="text" class="message-textarea" name="" id="" cols="30" rows="10" v-model="message" @keyup.enter.prevent="send">
             </textarea>
             <button class="message-submit-btn" @click.prevent="send"></button>
         </div>
@@ -47,8 +48,45 @@ export default {
         console.log(err)
       }
     },
+    scrollToBottom(){
+      const messageContent = document.querySelector('.message-content');
+      messageContent.scrollTop = messageContent.scrollHeight;
+    },
     showMessage : function(data){
       this.receive = data.text 
+      const chatBox = document.createElement('div')
+      chatBox.style.marginTop = '10px';
+      chatBox.style.marginRight = '20px';
+      chatBox.style.textAlign = 'right';
+      
+      
+      const chatText = document.createElement('div')
+      chatText.style.display = 'inline-block';
+      chatText.style.position = 'relative';
+      chatText.style.backgroundColor = '#486880';
+      chatText.style.borderRadius = '20px 0px 20px 20px';
+      chatText.style.color = '#fff';
+      chatText.style.padding = '10px 15px';
+      chatText.style.marginBottom = '10px;'
+      chatText.style.maxWidth = '230px'
+      chatText.innerHTML = this.receive
+      document.querySelector('.message-content').appendChild(chatBox)
+
+      const chatTime = document.createElement('div')
+      chatTime.style.position = 'absolute';
+      chatTime.style.left = '-55px';
+      chatTime.style.top = '15px';
+      chatTime.style.color = '#B1AFAF';
+      chatTime.style.fontSize = '12px'
+      chatTime.innerHTML = '오후 2:43';
+      chatBox.appendChild(chatText)
+      chatText.appendChild(chatTime)
+
+      this.scrollToBottom();
+
+
+
+
     },
     callBack : function(){
       // console.log(data)
@@ -101,13 +139,7 @@ export default {
   mounted(){
     this.startPage()
   },
-  watch:{
-    receive(newValue, oldValue){
-      const div = document.createElement('div')
-      div.innerHTML = newValue
-      document.querySelector('.message-content').appendChild(div)
-    }
-  }
+
 }
 </script>
 
