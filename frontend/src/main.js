b44a23d83f307a555638e1135120cf38250b0ade
hyunjihye <hhh259@naver.com>
2024-02-04 21:28:34 +0900

    feat: 마이페이지 반응형 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,8 @@ import { createMetaManager } from 'vue-meta'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 import mitt from 'mitt'
+import VueCarousel from 'vue-carousel';
+
 
 // import socket from 'vue3-websocket'
 let emitter = mitt();
@@ -15,6 +17,7 @@ const app = createApp(App)
 app.use(router)
 app.use(pinia)
 app.use(createMetaManager())
+app.use(VueCarousel);
 app.config.globalProperties.emitter = emitter
     // .use(socket, 'ws-stomp')
 app.mount('#app')
