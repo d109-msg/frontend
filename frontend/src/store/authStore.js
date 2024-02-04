7ba26b1d365f788d3c433053df2fe1b4c7fb2fef
hyunjihye <hhh259@naver.com>
2024-02-04 12:15:29 +0900

    feat: 게임 페이지 반응형 조정

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -6,8 +6,11 @@ import axios from "axios"
 
 
 const cookies = useCookies().cookies
-const server =  'https://i10d109.p.ssafy.io/api'
-const server2 = 'http://localhost:8080/api'
+const server =  'http://localhost:8080/api'
+const server2 = 'https://i10d109.p.ssafy.io/api'
+// const server =  'https://i10d109.p.ssafy.io/api'
+// const server2 = 'http://localhost:8080/api'
+
 
 export const useAuthStore = defineStore('auth',{
     state: ()=>({
