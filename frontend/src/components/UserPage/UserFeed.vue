ee76db044a29fb4a61e8a1e9cae856c26707d750
suehwanBoo <rndjdieo119@naver.com>
2024-02-04 21:29:40 +0900

    feat : userpage제작

---
+++ frontend/src/components/UserPage/UserFeed.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="myfeed-box">
+    <p class="myfeed-title"> 
+      <img src="./Img/icon_myfeed.png" alt="">
+      My Feed
+    </p>
+    <div class="myfeed-content" >
+      <div class="content" v-for="(item,key) in myFeed" :key="key">
+        <img :src="item.url" class="img" @click="onDetail(item)">
+      </div>
+    </div>
+    <DetailPage v-if="detailFlag"
+        @close-detail="offDetail"
+        :idx="detail"
+        />
+  </div>
+</template>
+
+<script>
+import { useAuthStore } from '@/store/authStore'
+import { useFeedStore } from '@/store/feedStore'
+import DetailPage from '../DetailPage/DetailPage.vue'
+export default {
+    name: 'UserFeed',
+    data(){
+      return{
+        myFeed : [],
+        myInfo : {},
+        detailFlag : false,
+        detail : {}
+      }
+    },
+    methods:{
+      getFeed : async function(){
+      const feed = useFeedStore()
+      try{
+        let value = await feed.getUserProfile(this.myInfo.id)
+        this.myFeed = value.data
+        console.log(this.myFeed)
+      } catch(error) {
+        console.log(error)
+      }
+    },
+      offDetail : function(){
+        this.detailFlag = false
+      },
+      onDetail : function(item){
+        this.detail = item
+        this.detailFlag = true
+      }
+    },
+    components:{
+      DetailPage
+    },
+    mounted(){
+      const auth = useAuthStore()
+      this.myInfo = auth.getUserInfo
+      this.getFeed()
+    },
+    
+    
+}
+</script>
+
+<style scoped src="./css/MyFeed.css">
+
+</style>
