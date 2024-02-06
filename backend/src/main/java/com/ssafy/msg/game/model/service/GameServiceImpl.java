3a72a9dfd620fc87c31d8721b2f56efbd31f9537
SSAFY <cnn482@gmail.com>
2024-02-06 13:03:44 +0900

    feat: 미션 게시물 작성시 미션성공 체크

--- backend/src/main/java/com/ssafy/msg/game/model/service/GameServiceImpl.java
+++ backend/src/main/java/com/ssafy/msg/game/model/service/GameServiceImpl.java
@@ -99,7 +99,7 @@ public class GameServiceImpl implements GameService{
             return -1;
         }
 
-        int participantId = participant.getUserId();
+        int participantId = participant.getId();
         Integer day = getMaxDay(participantId);
         
         //죽었는지 체크
