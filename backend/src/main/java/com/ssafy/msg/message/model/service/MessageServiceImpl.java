6167b4a42066a19aa6b49cc9dd883f27703aaa14
금예인 <tbc07061@naver.com>
2024-02-07 09:50:09 +0900

    feat: 텍스트 메시지 전송 MongoDB 저장

--- backend/src/main/java/com/ssafy/msg/message/model/service/MessageServiceImpl.java
+++ backend/src/main/java/com/ssafy/msg/message/model/service/MessageServiceImpl.java
@@ -1,14 +1,12 @@
 package com.ssafy.msg.message.model.service;
 
 import com.ssafy.msg.article.util.S3Util;
-import com.ssafy.msg.game.model.dto.EnterGroupRoomDto;
 import com.ssafy.msg.game.model.dto.ParticipantDto;
-import com.ssafy.msg.message.model.dto.ImageMessageDto;
-import com.ssafy.msg.message.model.dto.MessageRequestDto;
-import com.ssafy.msg.message.model.dto.MessageResponseDto;
-import com.ssafy.msg.message.model.dto.TextMessageDto;
+import com.ssafy.msg.message.model.dto.*;
 import com.ssafy.msg.message.model.entity.MessageEntity;
 import com.ssafy.msg.message.model.mapper.MessageMapper;
+import com.ssafy.msg.message.model.repo.MessageRepository;
+import com.ssafy.msg.message.util.DateTimeUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -24,51 +22,43 @@ public class MessageServiceImpl implements MessageService{
 
     private final SimpMessageSendingOperations sendingOperations;
     private final S3Util s3Util;
+    private final DateTimeUtil dateTimeUtil;
+    private final MessageRepository messageRepository;
     private final MessageMapper messageMapper;
 
     @Override
     public void sendTextMessage(TextMessageDto textMessageDto, int userId) {
-        // DB 저장 로직 구현 필요
-//        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
-//                .id(0)
-//                .roomId(textMessageDto.getRoomId())
-//                .userId(userId)
-//                .sendTime("")
-//                .dataType("text")
-//                .text(textMessageDto.getText()).build();
-//        sendingOperations.convertAndSend("/sub/"+messageResponseDto.getRoomId(), messageResponseDto);
+        MessageEntity messageEntity = MessageEntity.builder()
+                .roomId(textMessageDto.getRoomId())
+                .userId(userId)
+                .flagMafia(textMessageDto.getFlagMafia())
+                .dataType("chat")
+                .createTime(dateTimeUtil.getCurrentDateTime())
+                .content(textMessageDto.getText()).build();
 
+        messageRepository.save(messageEntity);
 
-//
-//        MessageRequestDto messageRequestDto = MessageRequestDto.builder()
-//                .userId(userId)
-//                .roomId(textMessageDto.getRoomId())
-//                .content(textMessageDto.getText()).build();
-//
-//        // MessageRequestDto를 MessageEntity로 변환
-//        MessageEntity messageEntity = messageRequestDto.toEntity();
-//        messageEntity.setCreateTime(dateTimeUtil.getCurrentDateTime());
-//
-//        // messageRepository를 이용하여 messageEntity를 저장
-//        messageRepository.save(messageEntity);
-//        log.info(String.valueOf(messageEntity));
-//
-//        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
-//                .id(messageEntity.getId())
-//                .roomId(messageEntity.getRoomId())
-//                .userId(messageEntity.getUserId())
-//                .flagMafia(messageEntity.getFlagMafia())
-//                .sendTime(messageEntity.getCreateTime())
-//                .dataType("text")
-//                .text(messageEntity.getContent()).build();
-//
-//        sendingOperations.convertAndSend("/sub/"+messageResponseDto.getRoomId(), messageResponseDto);
+        sendingOperations.convertAndSend("/sub/"+messageEntity.toDto().getRoomId(), messageEntity.toDto());
+        log.info(messageEntity.toDto().getRoomId() + " - " + messageEntity.toDto().getContent());
     }
 
     @Override
     public void sendImageMessage(ImageMessageDto imageMessageDto, int userId) throws IOException {
         String uuid = s3Util.saveFile(imageMessageDto.getImage());
         String url = s3Util.getUrl(uuid);
+
+        MessageEntity messageEntity = MessageEntity.builder()
+                .roomId(imageMessageDto.getRoomId())
+                .userId(userId)
+                .flagMafia(imageMessageDto.getFlagMafia())
+                .dataType("chat")
+                .createTime(dateTimeUtil.getCurrentDateTime())
+                .build();
+
+        MessageImageDto messageImageDto = MessageImageDto.builder()
+                .uuid(uuid)
+                .url(url)
+                .build();
 
         // DB 저장 로직 구현 필요
 //        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
@@ -84,102 +74,98 @@ public class MessageServiceImpl implements MessageService{
 
     @Override
     public void sendEnterNotice(ParticipantDto participantDto){
-        // DB 저장 로직 구현 필요
-        // Time 추가 로직 구현 필요
-//        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
-//                .id(0)
-//                .roomId(participantDto.getRoomId())
-//                .userId(0)
-//                .noticeType("enterNotice")
-//                .sendTime("")
-//                .dataType("text")
-//                .text(participantDto.getNickname()+"님이 입장하였습니다.").build();
-//        sendingOperations.convertAndSend("/sub/"+messageResponseDto.getRoomId(), messageResponseDto);
-//        log.info(messageResponseDto.getRoomId() + " - " + messageResponseDto.getText());
+        MessageEntity messageEntity = MessageEntity.builder()
+                .roomId(participantDto.getRoomId())
+                .userId(1)
+                .dataType("notice")
+                .noticeType("enter")
+                .createTime(dateTimeUtil.getCurrentDateTime())
+                .content(participantDto.getNickname()+"님이 입장하였습니다.").build();
+
+        messageRepository.save(messageEntity);
+
+        sendingOperations.convertAndSend("/sub/"+messageEntity.toDto().getRoomId(), messageEntity.toDto());
+        log.info(messageEntity.toDto().getRoomId() + " - " + messageEntity.toDto().getContent());
     }
 
     @Override
     public void sendInvitation(int userId, String chatRoomId, String gameRoomId) {
-        // DB 저장 로직 구현 필요
-        // Time 추가 로직 구현 필요
-//        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
-//                .id(0)
-//                .roomId(chatRoomId)
-//                .userId(userId)
-//                .noticeType("invitation")
-//                .sendTime("")
-//                .dataType("text")
-//                .text("초대 코드를 확인하세요. " + gameRoomId).build();
-//        sendingOperations.convertAndSend("/sub/"+messageResponseDto.getRoomId(), messageResponseDto);
-//        log.info(messageResponseDto.getRoomId() + " - " + messageResponseDto.getText());
+        MessageEntity messageEntity = MessageEntity.builder()
+                .roomId(chatRoomId)
+                .userId(userId)
+                .dataType("invite")
+                .createTime(dateTimeUtil.getCurrentDateTime())
+                .content("초대 코드를 확인하세요. " + gameRoomId).build();
+
+        messageRepository.save(messageEntity);
+
+        sendingOperations.convertAndSend("/sub/"+messageEntity.toDto().getRoomId(), messageEntity.toDto());
+        log.info(messageEntity.toDto().getRoomId() + " - " + messageEntity.toDto().getContent());
     }
 
     @Override
     public void sendDayNotice(String dayOrNight, String roomId) throws SQLException {
-        // DB 저장 로직 구현 필요
-        // Time 추가 로직 구현 필요
-//        int day = messageMapper.calDay(roomId) + 1;
-//        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
-//                .id(0)
-//                .roomId(roomId)
-//                .userId(0)
-//                .noticeType("gameNotice")
-//                .sendTime("")
-//                .dataType("text")
-//                .text(day+"일차 " + dayOrNight + "이 되었습니다.").build();
-//        sendingOperations.convertAndSend("/sub/"+messageResponseDto.getRoomId(), messageResponseDto);
-//
-//        log.info(messageResponseDto.getRoomId() + " - " + messageResponseDto.getText());
+        int day = messageMapper.calDay(roomId) + 1;
+
+        MessageEntity messageEntity = MessageEntity.builder()
+                .roomId(roomId)
+                .userId(1)
+                .dataType("notice")
+                .noticeType("day")
+                .createTime(dateTimeUtil.getCurrentDateTime())
+                .content(day+"일차 " + dayOrNight + "이 되었습니다.").build();
+
+        messageRepository.save(messageEntity);
+
+        sendingOperations.convertAndSend("/sub/"+messageEntity.toDto().getRoomId(), messageEntity.toDto());
+        log.info(messageEntity.toDto().getRoomId() + " - " + messageEntity.toDto().getContent());
     }
 
     @Override
     public void sendStartNotice(String roomId) throws SQLException {
-        // DB 저장 로직 구현 필요
-        // Time 추가 로직 구현 필요
-//        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
-//                .id(0)
-//                .roomId(roomId)
-//                .userId(0)
-//                .noticeType("gameNotice")
-//                .sendTime("")
-//                .dataType("text")
-//                .text("게임이 시작되었습니다.").build();
-//        sendingOperations.convertAndSend("/sub/"+messageResponseDto.getRoomId(), messageResponseDto);
-//
-//        log.info(messageResponseDto.getRoomId() + " - " + messageResponseDto.getText());
+        MessageEntity messageEntity = MessageEntity.builder()
+                .roomId(roomId)
+                .userId(1)
+                .dataType("notice")
+                .noticeType("game")
+                .createTime(dateTimeUtil.getCurrentDateTime())
+                .content("게임이 시작되었습니다.").build();
+
+        messageRepository.save(messageEntity);
+
+        sendingOperations.convertAndSend("/sub/"+messageEntity.toDto().getRoomId(), messageEntity.toDto());
+        log.info(messageEntity.toDto().getRoomId() + " - " + messageEntity.toDto().getContent());
     }
 
     @Override
     public void sendEndNotice(String roomId) throws SQLException {
-        // DB 저장 로직 구현 필요
-        // Time 추가 로직 구현 필요
-//        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
-//                .id(0)
-//                .roomId(roomId)
-//                .userId(0)
-//                .noticeType("gameNotice")
-//                .sendTime("")
-//                .dataType("text")
-//                .text("게임이 종료되었습니다.").build();
-//        sendingOperations.convertAndSend("/sub/"+messageResponseDto.getRoomId(), messageResponseDto);
-//
-//        log.info(messageResponseDto.getRoomId() + " - " + messageResponseDto.getText());
+        MessageEntity messageEntity = MessageEntity.builder()
+                .roomId(roomId)
+                .userId(1)
+                .dataType("notice")
+                .noticeType("game")
+                .createTime(dateTimeUtil.getCurrentDateTime())
+                .content("게임이 종료되었습니다.").build();
+
+        messageRepository.save(messageEntity);
+
+        sendingOperations.convertAndSend("/sub/"+messageEntity.toDto().getRoomId(), messageEntity.toDto());
+        log.info(messageEntity.toDto().getRoomId() + " - " + messageEntity.toDto().getContent());
     }
 
     @Override
     public void sendGameNotice(String roomId, String text) throws SQLException {
-        // DB 저장 로직 구현 필요
-        // Time 추가 로직 구현 필요
-//        MessageResponseDto messageResponseDto = MessageResponseDto.builder()
-//                .id(0)
-//                .roomId(roomId)
-//                .userId(0)
-//                .noticeType("gameNotice")
-//                .sendTime("")
-//                .dataType("text")
-//                .text(text).build();
-//        sendingOperations.convertAndSend("/sub/"+messageResponseDto.getRoomId(), messageResponseDto);
-//
-//        log.info(messageResponseDto.getRoomId() + " - " + messageResponseDto.getText());
+        MessageEntity messageEntity = MessageEntity.builder()
+                .roomId(roomId)
+                .userId(1)
+                .dataType("notice")
+                .noticeType("game")
+                .createTime(dateTimeUtil.getCurrentDateTime())
+                .content(text).build();
+
+        messageRepository.save(messageEntity);
+
+        sendingOperations.convertAndSend("/sub/"+messageEntity.toDto().getRoomId(), messageEntity.toDto());
+        log.info(messageEntity.toDto().getRoomId() + " - " + messageEntity.toDto().getContent());
     }
 }
