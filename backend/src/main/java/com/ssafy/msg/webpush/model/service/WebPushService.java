07ede757ca94fb10506d9c414a08a23d88eba392
금예인 <tbc07061@naver.com>
2024-02-15 15:32:36 +0900

    fix: web push 수정

--- backend/src/main/java/com/ssafy/msg/webpush/model/service/WebPushService.java
+++ backend/src/main/java/com/ssafy/msg/webpush/model/service/WebPushService.java
@@ -2,6 +2,7 @@ package com.ssafy.msg.webpush.model.service;
 
 import com.ssafy.msg.article.model.dto.CommentDto;
 import com.ssafy.msg.webpush.model.dto.FCMTokenDto;
+import org.springframework.scheduling.annotation.Async;
 
 public interface WebPushService {
 
